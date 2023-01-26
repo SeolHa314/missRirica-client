51b09fd1a1d55ea5311193cd0df604100c3f569d
果物リン <nassii74@gmail.com>
2023-01-26 11:43:15 +0900

    Dev/1.5 (#25)
    * init
    
    * init
    
    * assets
    
    * v13 migrate
    
    * v13 migrate
    
    * v13 migrate
    
    * v13 migrate
    
    * v13 migrate
    
    * [from now] 2023/01/25 05:34:05
    
    diff --git a/android/app/build.gradle b/android/app/build.gradle
    index 40b18207b..4a6bd1e1e 100644
    --- a/android/app/build.gradle
    +++ b/android/app/build.gradle
    @@ -6,7 +6,7 @@ android {
             applicationId "space.riinswork.missririca"
             minSdkVersion rootProject.ext.minSdkVersion
             targetSdkVersion rootProject.ext.targetSdkVersion
    -        versionCode 15
    +        versionCode 16
             versionName "1.5.0"
             testInstrumentationRunner "androidx.test.runner.AndroidJUnitRunner"
             aaptOptions {
    
    * fix reaction picker
    
    * fix sound
    
    * fix css
    
    * fix css
    
    * fix css
    
    * fix sound
    
    * Fix volume (#28)
    
    * fix volume to actually change
    
    * Fix indentations
    
    Co-authored-by: Thomas Shin <40733603+SeolHa314@users.noreply.github.com>

--- src/stream.ts
+++ src/stream.ts
@@ -1,14 +1,10 @@
-import * as Misskey from "misskey-js";
-import { markRaw } from "vue";
-import { $i } from "@/account";
+import * as Misskey from 'misskey-js';
+import { markRaw } from 'vue';
+import { $i } from '@/account';
 
-export const stream = markRaw(
-  new Misskey.Stream(
-    $i ? $i.instanceUrl : "https://misskey.io",
-    $i
-      ? {
-          token: $i.token,
-        }
-      : null
-  )
-);
+export const stream = $i ? markRaw(
+		new Misskey.Stream($i.instanceUrl, {
+				token: $i?.token,
+			}
+	)
+): null;
